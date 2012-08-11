3ceca27fa9d2bab5f9db4aef562a60a386b45740
esfand <esfand@wow.com>
2012-08-11 15:26:09 +0300

    Added push.cmd and pull.cmd files.

---
+++ pull.cmd
@@ -0,0 +1,61 @@
+cd ..
+
+cd github-es4j-all
+call git.cmd pull
+cd ..
+
+cd github-es4j-es4j-util
+call git.cmd pull
+cd ..
+
+cd github-es4j-es4j-dotnet
+call git.cmd pull
+cd ..
+
+cd github-es4j-es4j-container
+call git.cmd pull
+cd ..
+
+cd github-es4j-es4j-messaging
+call git.cmd pull
+cd ..
+
+cd github-es4j-es4j-eventstore-api
+call git.cmd pull
+cd ..
+
+cd github-es4j-es4j-eventstore-core
+call git.cmd pull
+cd ..
+
+cd github-es4j-es4j-eventstore-example
+call git.cmd pull
+cd ..
+
+cd github-es4j-es4j-eventstore-wireup
+call git.cmd pull
+cd ..
+
+cd github-es4j-es4j-persistence-acceptance
+call git.cmd pull
+cd ..
+
+cd github-es4j-es4j-persistence-sql
+call git.cmd pull
+cd ..
+
+cd github-es4j-es4j-serialization-api
+call git.cmd pull
+cd ..
+
+cd github-es4j-es4j-serialization-json
+call git.cmd pull
+cd ..
+
+cd github-es4j-es4j-serialization-main
+call git.cmd pull
+cd ..
+
+
+
+
---
+++ push.cmd
@@ -0,0 +1,9 @@
+cd \repos-es4j
+
+cd github-es4j-es4j-util
+git.exe add .
+git.exe commit -m"Switching PCs."
+git.exe push
+cd ..
+
+
